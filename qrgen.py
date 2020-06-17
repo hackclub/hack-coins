abb335d6a63dabc1e6cfc0e480511c4331d73e25
lukec11 <me@lukec.me>
2020-06-17 16:42:59 -0400

    update to env vars in qrgen

--- qrgen.py
+++ qrgen.py
@@ -3,18 +3,18 @@ import uuid
 import requests
 import json
 from airtable import Airtable
+import os
 
-#Getting config stuff
-with open("config.json") as f:
-    config = json.load(f);
-    airtable_auth_key = config["airtable_auth_key"]
-    base_key = config["base_key"] 
+# Getting environ vars
+airtable_auth_key = os.environ["airtable_auth_key"]
+base_key = os.environ["base_key"]
+
 
 class GPQRGen():
     def __init__(self, domain, amount=20):
         self.newUUID = str(uuid.uuid4())
-        
-        #Writes the info to the Airtable base
+
+        # Writes the info to the Airtable base
         airtable = Airtable(base_key, "Coin Claims", api_key=airtable_auth_key)
         record = {
             "UUID": self.newUUID,
@@ -22,16 +22,16 @@ class GPQRGen():
             "Claimant Slack Email": ""
         }
         airtable.insert(record)
-        
-        #Creating the link
+
+        # Creating the link
         self.link = f"{domain}/claim?uuid={self.newUUID}&amount={amount}"
-    
+
     def getlink(self):
         return self.link
 
     def getUUID(self):
         return self.newUUID
-    
+
     def generate(self):
         img = qrcode.make(self.getlink())
         img.save(f"static/{self.getUUID()}.png")
